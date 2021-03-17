159f0c0428c056e43a5e90d274459e93037ac885
christopher <christopher.t.yourth@gmail.com>
2021-03-17 14:04:06 -0600

    gathered Recipe API Id and Key from Edamam.com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 
 function App() {
+
+  const APP_ID = "89d94235";
+  const APP_KEY = "534773668dae75297f457a5109a2a95c";
+
+
+  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+
   return (
     <div className="App">
       <h1>Hello React!</h1>
